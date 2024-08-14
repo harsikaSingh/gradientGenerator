ba163b835dc453ccc68158d12d25fa267d17f4c8
harsikaSingh <harsika786@gmail.com>
2024-08-14 23:51:39 +0530

    Add javaScript and css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let btn1 = document.getElementById("myButton1");
 let btn2 = document.getElementById("myButton2");
 let copyDiv = document.querySelector(".copyCode");
-let rgb1 = "rgb(23,244,123)";
-let rgb2 = "rgb(34,65,234)";
+let rgb1 = "#17f47b";
+let rgb2 = "#2241ea";
 
 const hexValues = () => {
     let myHexValues = "0123456789abcdef";
     let color = "#";
 
-    for(let i =0;i<6;i++){
-       color = color + myHexValues[Math.floor(Math.random()*16)]
+    for(let i = 0; i < 6; i++){
+        color += myHexValues[Math.floor(Math.random() * 16)];
     }
     return color;
 }
@@ -18,22 +18,21 @@ const handleButton1 = () => {
     rgb1 = hexValues();
     console.log(rgb1);
     btn1.innerText = rgb1;
-    document.body.style.backgroundImage= `linear gradient(to right, ${rgb1}, ${rgb2})`;
-    copyDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});
-`
+    document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    copyDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});`;
 };
+
 const handleButton2 = () => {
-    rgb2  = hexValues();
+    rgb2 = hexValues();
     console.log(rgb2);
     btn2.innerText = rgb2;
-    document.body.style.backgroundImage= `linear gradient(to right, ${rgb1}, ${rgb2})`;
-    copyDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});
-`
+    document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    copyDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});`;
 };
 
-btn1.addEventListener("click",handleButton1);
-btn2.addEventListener("click",handleButton2);
+btn1.addEventListener("click", handleButton1);
+btn2.addEventListener("click", handleButton2);
 
-copyDiv.addEventListener('click',() => {
+copyDiv.addEventListener('click', () => {
     navigator.clipboard.writeText(copyDiv.innerText);
-} )
+});
